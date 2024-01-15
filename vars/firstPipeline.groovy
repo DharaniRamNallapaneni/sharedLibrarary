cd5ba28458d09efc15b6ee489102446351fe4fa6
DharaniRamNallapaneni <121327549+DharaniRamNallapaneni@users.noreply.github.com>
2024-01-15 08:06:42 +0530

    add extra logic to vars/firstPipeline.groovy

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -1,12 +1,20 @@
 import com.i27.builds.Calc
-//Constructor
-Calc cal = new Calc(this)
-pipeline {
-    agent any
-    stages {
+def call(Map pipelineParams){
+    // this value is the one we given in jenkins
+    library("com.i27.slb@main")
+    //Constructor
+    Calc cal = new Calc(this)
+     pipeline {
+      agent any
+      environment {
+        // here this pipeline is expecting a value from service who ever is calling this pipeline
+        APP_NAME = "${pipelineParams.appName}"
+      }
+       stages {
         stage ('Add') {
             steps {
                 script {
+                  echo "MicroService name is ${env.APP_NAME}"
                   echo "Printing sum of 2 numbers"
                   println cal.add(3,4)
                 }
@@ -15,10 +23,12 @@ pipeline {
         stage ('Multiply') {
             steps {
                 script {
+                  echo "MicroService name is ${env.APP_NAME}"
                   echo "Multiplying 2 numbers"
                   println cal.multiply(3,4)
                 }
             }
         }
     }
+ }
 }
